bc8a4bfeeb03aec167bab51fae3f440e3143c087
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2022-03-02 15:56:21 +0100

    calc

--- test/100-routes.js
+++ test/100-routes.js
@@ -7,70 +7,77 @@ var Server = require('../server');
 var routes = require('../lib/routes');
 
 describe('POST /order', function () {
-  var $routes;
-  beforeEach(function () {
-    sinon.spy(routes, 'order')
-  });
-  afterEach(function () {
-    routes.order.restore();
-  })
+    var $routes;
+    // beforeEach(function () {
+    //     sinon.spy(routes, 'order');
+    // });
+    // afterEach(function () {
+    //     routes.order.restore();
+    // });
 
-  var supertest = startServer();
+    var supertest = startServer();
 
-  it('calls routes.order(req, res, next) with parsed body', function (done) {
-    supertest()
-      .post('/order')
-      .send({some: 'body'})
-      .end(function (err) {
-        if (err) return done(err);
-        expect(routes.order).to.have.been.calledWith(sinon.match({body: {some: 'body'}}));
-        done()
-      })
-  })
-})
+    it('calls routes.order(req, res, next) with parsed body', function (done) {
+        supertest()
+            .post('/order')
+            .send({
+                prices: [16.54, 13.11],
+                quantities: [7, 3],
+                country: 'UK',
+                reduction: 'PAY THE PRICE',
+            })
+            .end(function (err, result) {
+                if (err) return done(err);
+                expect(result.body.total).to.equal(187.68310000000002);
+                done();
+            });
+    });
+});
 
 describe('POST /feedback', function () {
-  var $routes;
-  beforeEach(function () {
-    sinon.spy(routes, 'feedback')
-  });
-  afterEach(function () {
-    routes.feedback.restore();
-  })
+    var $routes;
+    beforeEach(function () {
+        sinon.spy(routes, 'feedback');
+    });
+    afterEach(function () {
+        routes.feedback.restore();
+    });
 
-  var supertest = startServer();
+    var supertest = startServer();
 
-  it('calls routes.feedback(req, res, next) with parsed body', function (done) {
-    supertest()
-      .post('/feedback')
-      .send({my: 'feedback'})
-      .end(function (err) {
-        if (err) return done(err);
-        expect(routes.feedback).to.have.been.calledWith(sinon.match({body: {my: 'feedback'}}));
-        done()
-      })
-  })
-})
+    it('calls routes.feedback(req, res, next) with parsed body', function (done) {
+        supertest()
+            .post('/feedback')
+            .send({ my: 'feedback' })
+            .end(function (err) {
+                if (err) return done(err);
+                expect(routes.feedback).to.have.been.calledWith(
+                    sinon.match({ body: { my: 'feedback' } })
+                );
+                done();
+            });
+    });
+});
 
-function startServer () {
-  beforeEach(function () {
-    previousPort = process.env.PORT;
-    process.env.PORT = 21345;
-  })
-  afterEach(function () {
-    process.env.PORT = previousPort;
-  });
+function startServer() {
+    beforeEach(function () {
+        previousPort = process.env.PORT;
+        process.env.PORT = 21345;
+    });
+    afterEach(function () {
+        process.env.PORT = previousPort;
+    });
 
-  beforeEach(function (done) {
-    server = new Server();
-    server.start(done);
-  })
+    beforeEach(function (done) {
+        server = new Server();
+        server.start(done);
+    });
 
-  afterEach(function (done) {
-    server.stop(done);
-  })
+    afterEach(function (done) {
+        server.stop(done);
+    });
 
-  return function () {
-    return supertest('http://localhost:21345');
-  }
+    return function () {
+        return supertest('http://localhost:21345');
+    };
 }
--- test/200-process-order.js
+++ test/200-process-order.js
@@ -2,11 +2,130 @@ var expect = require('chai').expect;
 var process = require('../lib/process');
 
 describe('process.order(payload, callback)', function () {
-  it('calls back with an empty object', function (done) {
-    process.order({}, function (err, result) {
-      if (err) return done(err);
-      expect(result).to.deep.equal({});
-      done();
-    })
-  })
-})
+    describe('subtotal1', () => {
+        it('calls back with an empty object', function (done) {
+            process.order({}, function (err, result) {
+                if (err) return done(err);
+                expect(result).to.deep.equal({});
+                done();
+            });
+        });
+
+        it('calls back with prices summed', function (done) {
+            process.order(
+                { prices: [10], quantities: [1], country: 'FAKE' },
+                function (err, result) {
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(10);
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed2', function (done) {
+            process.order(
+                { prices: [10], quantities: [3], country: 'FAKE' },
+                function (err, result) {
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(10 * 3);
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed3', function (done) {
+            process.order(
+                {
+                    prices: [10, 20, 30],
+                    quantities: [1, 1, 1],
+                    country: 'FAKE',
+                },
+                function (err, result) {
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(10 + 20 + 30);
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed4', function (done) {
+            process.order(
+                {
+                    prices: [10, 20, 30],
+                    quantities: [2, 4, 1],
+                    country: 'FAKE',
+                },
+                function (err, result) {
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(10 * 2 + 20 * 4 + 30);
+                    done();
+                }
+            );
+        });
+    });
+
+    describe('subtotal2', () => {
+        it('calls back with prices summed', function (done) {
+            process.order(
+                { prices: [10], quantities: [1], country: 'DE' },
+                function (err, result) {
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(10 + (10 * 20) / 100);
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed2', function (done) {
+            process.order(
+                { prices: [10], quantities: [3], country: 'UK' },
+                function (err, result) {
+                    if (err) return done(err);
+                    const subtotal = 10 * 3;
+                    expect(result).to.deep.equal(
+                        subtotal + (subtotal * 21) / 100
+                    );
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed3', function (done) {
+            process.order(
+                {
+                    prices: [10, 20, 30],
+                    quantities: [1, 1, 1],
+                    country: 'FR',
+                },
+                function (err, result) {
+                    if (err) return done(err);
+                    const subtotal = 10 + 20 + 30;
+                    expect(result).to.deep.equal(
+                        subtotal + (subtotal * 20) / 100
+                    );
+                    done();
+                }
+            );
+        });
+
+        it('calls back with prices summed4', function (done) {
+            process.order(
+                {
+                    prices: [10, 20, 30],
+                    quantities: [2, 4, 1],
+                    country: 'IT',
+                },
+                function (err, result) {
+                    const subtotal = 10 * 2 + 20 * 4 + 30;
+                    if (err) return done(err);
+                    expect(result).to.deep.equal(
+                        subtotal + (subtotal * 25) / 100
+                    );
+                    done();
+                }
+            );
+        });
+    });
+
+    describe('subtotal3', () => {});
+});
